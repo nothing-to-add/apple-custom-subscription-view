d25862f45271f8adf9d58a5e0dced59df8049402
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2025-09-14 02:14:12 +0300

    initial setup of the package

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "apple-custom-subscription-view",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .watchOS(.v9),
+        .visionOS(.v1)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CustomSubscription",
+            targets: ["CustomSubscription"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nothing-to-add/apple-custom-toast-manager.git", from: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CustomSubscription",
+            dependencies: [
+                .product(name: "CustomToastManager", package: "apple-custom-toast-manager")
+            ]),
+
+    ]
+)
